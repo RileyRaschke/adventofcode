feb8689b066b17ab9be0188cf6f59c4f72a44f78
Riley Raschke <riley@rrappsdev.com>
2020-12-13 11:22:32 -0600

    Prep for d10p2

--- 2020/day10/main.go
+++ 2020/day10/main.go
@@ -23,11 +23,17 @@ func main() {
     }
     // Add the outlet
     adapters = append(adapters,0)
-
     sort.Ints(adapters)
-    tracker := make(map[int]int)
+    // Add the device
+    adapters = append(adapters, adapters[len(adapters)-1]+3)
 
-    // brute with mini optimization
+    p1_tracker := p1(adapters)
+    fmt.Printf("Part1:\n\t1-jolt diff: %v\n\t3-jolt diff: %v\n\tmultiple: %v\n", p1_tracker[1], p1_tracker[3], p1_tracker[1]*p1_tracker[3])
+}
+
+func p1(adapters []int) (map[int]int){
+    tracker := make(map[int]int)
+    // p1 - brute with mini optimization
     for i:=0; i < len(adapters); i++ {
         for j:=i+1; j < i+4 && j<len(adapters); j++ {
             diff := adapters[j] - adapters[i]
@@ -41,8 +47,6 @@ func main() {
             }
         }
     }
-    // Add the computer
-    tracker[3]++
-    fmt.Printf("Part1:\n\t1-jolt diff: %v\n\t3-jolt diff: %v\n\tmultiple: %v\n", tracker[1], tracker[3], tracker[1]*tracker[3])
+    return tracker
 }
 
