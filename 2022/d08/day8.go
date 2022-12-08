1a5b8c7ea1d6c1e7e9d74fedb4595800b641ae6a
Riley Raschke <riley@rrappsdev.com>
2022-12-08 15:29:40 -0600

    Quick prune/optimize

--- 2022/d08/day8.go
+++ 2022/d08/day8.go
@@ -7,9 +7,9 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
-type ForestGrid [][]*Tree
 type Cord struct {
 	x int
 	y int
@@ -23,6 +23,8 @@ type Tree struct {
 	ScenicScore int
 }
 
+type ForestGrid [][]*Tree
+
 var (
 	grid ForestGrid
 )
@@ -35,6 +37,7 @@ func MakeRow(str string, row []*Tree) {
 }
 
 func main() {
+	start := time.Now()
 	grid = ForestGrid{}
 	reader := bufio.NewReader(os.Stdin)
 	for {
@@ -48,25 +51,20 @@ func main() {
 		MakeRow(line, row)
 		grid = append(grid, row)
 	}
-	grid.Search()
-	fmt.Printf("%v", grid)
-	exposed, maxScore := grid.Results()
+
+	exposed, maxScore := grid.Explore()
+
 	fmt.Printf("Part 1: %d trees are exposed\n", exposed)
 	fmt.Printf("Part 2: Highest senic score is %d\n", maxScore)
+	fmt.Printf("Total time: %s\n", time.Since(start))
 }
 
-func CordUp(c Cord) Cord {
-	return Cord{c.x, c.y - 1}
-}
-func CordDown(c Cord) Cord {
-	return Cord{c.x, c.y + 1}
-}
-func CordLeft(c Cord) Cord {
-	return Cord{c.x - 1, c.y}
-}
-func CordRight(c Cord) Cord {
-	return Cord{c.x + 1, c.y}
-}
+func CordUp(c Cord) Cord    { return Cord{c.x, c.y - 1} }
+func CordDown(c Cord) Cord  { return Cord{c.x, c.y + 1} }
+func CordLeft(c Cord) Cord  { return Cord{c.x - 1, c.y} }
+func CordRight(c Cord) Cord { return Cord{c.x + 1, c.y} }
+
+func (t *Tree) IsExposed() bool { return t.ExposedX || t.ExposedY }
 
 func (t *Tree) String() string {
 	var e, x, y int = 0, 0, 0
@@ -79,42 +77,37 @@ func (t *Tree) String() string {
 	if t.ExposedY {
 		y = 1
 	}
-	return fmt.Sprintf("%d-%d-%b%v%v", t.Height, t.ScenicScore, e, x, y)
+	return fmt.Sprintf("%d-%d-%d%d%d", t.Height, t.ScenicScore, e, x, y)
 }
 
-func (t *Tree) IsExposed() bool {
-	return t.ExposedX || t.ExposedY
-}
-
-func (g ForestGrid) Results() (int, int) {
-	var t int
+func (g ForestGrid) Explore() (int, int) {
+	var exposed int
 	var maxScore int
 	w := g.Width()
 	h := g.Height()
 	for x := 0; x < w; x++ {
 		for y := 0; y < h; y++ {
-			if g[y][x].IsExposed() {
-				t++
+			// go func() { //bombs out...
+			c := Cord{x, y}
+
+			g.FindExposed(c)
+			g.ScenicScores(c)
+
+			t := g.At(c)
+
+			if t.IsExposed() {
+				exposed++
 			}
-			if g[y][x].ScenicScore > maxScore {
-				maxScore = g[y][x].ScenicScore
+			if t.ScenicScore > maxScore {
+				maxScore = t.ScenicScore
 			}
+			//}()
 		}
 	}
-	return t, maxScore
+	return exposed, maxScore
 }
 
-func (g ForestGrid) Search() {
-	w := g.Width()
-	h := g.Height()
-	for x := 0; x < w; x++ {
-		for y := 0; y < h; y++ {
-			g.Explore(Cord{x, y})
-			g.ScenicScores(Cord{x, y})
-		}
-	}
-}
-func (g ForestGrid) Explore(c Cord) {
+func (g ForestGrid) FindExposed(c Cord) {
 	funcs := []func(Cord) Cord{CordUp, CordDown, CordLeft, CordRight}
 	yPlane := []bool{true, true, false, false}
 	this := g.At(c)
@@ -147,23 +140,23 @@ func (g ForestGrid) Explore(c Cord) {
 	}
 	this.Explored = true
 }
+
 func (g ForestGrid) ScenicScores(c Cord) {
 	funcs := []func(Cord) Cord{CordUp, CordDown, CordLeft, CordRight}
 	this := g.At(c)
 	for idx, f := range funcs {
-		dist := 1
+		dist := 0
 		nc := f(c)
 		for {
 			if !g.ValidCord(nc) {
-				dist--
 				break
 			}
+			dist++
 			n := g.At(nc)
 			if n.Height >= this.Height {
 				break
 			}
 			nc = f(nc)
-			dist++
 		}
 		if idx == 0 {
 			this.ScenicScore = dist
@@ -172,27 +165,18 @@ func (g ForestGrid) ScenicScores(c Cord) {
 		}
 	}
 }
+
+func (g ForestGrid) At(c Cord) *Tree { return g[c.y][c.x] }
+func (g ForestGrid) Width() int      { return len(g[0]) }
+func (g ForestGrid) Height() int     { return len(g) }
+
 func (g ForestGrid) ValidCord(c Cord) bool {
-	if c.x < 0 || c.y < 0 {
-		return false
-	}
-	if c.x >= g.Width() {
-		return false
-	}
-	if c.y >= g.Height() {
+	if c.x < 0 || c.y < 0 || c.x >= g.Width() || c.y >= g.Height() {
 		return false
 	}
 	return true
 }
-func (g ForestGrid) At(c Cord) *Tree {
-	return g[c.y][c.x]
-}
-func (g ForestGrid) Width() int {
-	return len(g[0])
-}
-func (g ForestGrid) Height() int {
-	return len(g)
-}
+
 func (g ForestGrid) String() string {
 	var x string
 	for _, row := range g {
